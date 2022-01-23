5ce9c59a8d6ff677063ea491d324334ec9256e31
anderson <anderson.d.cardoso92@gmail.com>
2022-01-23 11:57:16 -0600

    add header component styles

--- containers/Header.jsx
+++ containers/Header.jsx
@@ -1,7 +1,36 @@
 import headerStyles from '../styles/containers/header.module.css';
+import ai from '../public/ai.png';
+import people from '../public/people.png';
+import Image from 'next/image';
+
 
 const Header = () => {
-  return (<div></div>);
+  return (
+    <div className={`${headerStyles.gpt3__header} ${headerStyles.section__padding}`} id='home'>
+      <div className={headerStyles.gpt3__header_content}>
+        <h1 className={headerStyles.gradient__text}>
+          Let&apos;s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+        </p>
+
+        <div className={headerStyles.gpt3__header_content__input}>
+          <input type="email" placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+
+        <div className={headerStyles.gpt3__header_content__people}>
+          <Image src={people} alt='People' />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div>
+          <Image src={ai} alt='AI' />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import appStyles from '../styles/app.module.css';
 import Navbar from '../components/Navbar';
+import Header from '../containers/Header';
 
 const App = () => {
   return (
         <div className={appStyles.App}>
             <main className={appStyles.gradient__bg}>
                 <Navbar />
+                <Header />
             </main>
         </div>
     );
